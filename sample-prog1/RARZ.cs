299947d167af9c68d158bdfcb3ddd6aa16ab85f5
Ricardo Arturo <ricardoarturo2707@gmail.com>
2020-11-04 16:52:16 -0400

    Agregada la clase Ricardo

---
+++ sample-prog1/RARZ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sample_prog1
+{
+    public class RARZ
+    {
+        public string CreatorName = "Ricardo";
+        public int CreatorAge = 19;
+
+        public void ShowStudentData() 
+        {
+            Console.WriteLine("Student: Ricardo Ramirez\nID: 10139152\n");
+        }
+    }
+}
