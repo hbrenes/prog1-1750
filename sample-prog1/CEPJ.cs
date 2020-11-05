bda0ff33399edc09e9849b1c2d8b1014f9f02b7c
CarlosJ <carlosepjorge@gmail.com>
2020-11-04 22:36:06 -0400

    Ya

---
+++ sample-prog1/CEPJ.cs
@@ -0,0 +1,11 @@
+using System;
+namespace sample-prog1
+{
+    class Carlos
+    {
+        Static void Main(string[] args)
+        {
+            Console.WriteLine("Carlos Jorge!");
+        }
+    }
+}
