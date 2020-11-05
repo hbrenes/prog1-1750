63e31dfe78be19d90fc6432a75ec855a88e2f82e
Camilool8 <josejogag@live.com>
2020-11-05 09:10:32 -0400

    Update Camilito

--- sample-prog1/JCJG.cs
+++ sample-prog1/JCJG.cs
@@ -6,5 +6,12 @@ namespace sample_prog1
 {
     class JCJG
     {
+        String Nombre = " Jose Camilo Joga Guerrero";
+        int Id = 10138601;
+
+        public void Info()
+        {
+            Console.WriteLine($"El nombre del estudiante es: {Nombre}\n y su Id es: {Id}");
+        }
     }
 }
